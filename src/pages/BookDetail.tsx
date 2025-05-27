ab7a6b9546152ec0ef2cfb84807595de716a15bb
Mauricio Arroyo <mauricioarroyo@ascbomacmini76.local>
2025-05-27 17:08:35 -0400

    feat: added type to components

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -1,46 +1,16 @@
 import styles from "../styles/pages/BookDetail.module.css";
+import { books } from "../data/Books";
+import type { Book } from "../types/Book";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 export default function BookDetail() {
-  const books = [
-    {
-      id: "1",
-      title: "To Kill a Mockingbird",
-      author: "Harper Lee",
-      description: "A novel about racial injustice in the Deep South.",
-    },
-    {
-      id: "2",
-      title: "1984",
-      author: "George Orwell",
-      description: "A dystopian story about totalitarianism and surveillance.",
-    },
-    {
-      id: "3",
-      title: "Pride and Prejudice",
-      author: "Jane Austen",
-      description: "A classic romance exploring manners and marriage.",
-    },
-    {
-      id: "4",
-      title: "The Great Gatsby",
-      author: "F. Scott Fitzgerald",
-      description: "A story of wealth, love, and the American Dream.",
-    },
-    {
-      id: "5",
-      title: "Moby-Dick",
-      author: "Herman Melville",
-      description: "An epic tale of obsession and the sea.",
-    },
-  ];
   const { bookId } = useParams();
-  const [book, setBook] = useState<any>(null);
+  const [book, setBook] = useState<Book>(null!);
 
   useEffect(() => {
     const getBook = () => {
-      const foundBook = books.find((book) => book.id === bookId);
+      const foundBook = books.find((book) => book.id === bookId)!;
       setBook(foundBook);
     };
 
